bcd410a54bb43c498d011a3f1febdda9ce3f9aab
DevJohn <johnng87@protonmail.com>
2020-03-01 16:06:47 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import switchoff from './assets/images/switchoff.png';
+import bleep from './assets/audio/Bleep.mp3';
+
 import './App.css';
 
 function App() {
+  const [mode, setMode] = useState(false);
+
+
+  const toggleMode = () => {
+    const audio = document.getElementById("audio");
+    audio.pause();
+    audio.currentTime = 0;
+    mode ? setMode(false) : setMode(true)
+  }
+
+  const rndImg = () => {
+    const audio = document.getElementById("audio");
+    audio.play();
+    const val = Math.floor(Math.random() * (7 - 1 + 1)) + 1;
+    return (
+      <img src={require(`./assets/images/${val}.png`)}  className="imgdisplay"/>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <div className="App-header">
+      { mode ? rndImg() : <img src={switchoff}  className="imgdisplay"/>}
+      <div><button className={ mode ? "takebtnOn" : "takebtnOff"} onClick={toggleMode}></button></div>
+      
+      <audio id="audio">
+        <source src={bleep} />
+        Your browser does not support the <code>audio</code> element.
+      </audio>
+      </div>
+      
     </div>
   );
 }
